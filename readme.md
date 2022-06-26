ea5fc0e474e018bfa6c42e2e475ba4ed30fd9f7b
Sk0rpi0o <brown.stefan@gmail.com>
2022-06-26 14:51:59 -0400

    Version 1.2 // w/ add and remove notes

--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@ Notes App
   CRUD note
   mardown
   localStoraage
+
+    /*background: rgb(59,215,154);
+  background: radial-gradient(circle, rgba(59,215,154,1) 17%, rgba(214,178,240,1) 61%);
+  */
+
+
+    
--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-const notesEl = document.querySelector(".notes")
-const editBtn = document.querySelector(".edit");
-const deleteBtn = document.querySelector(".delete");
+const addBtn = document.getElementById("add");
 
-const main = notesEl.querySelector(".main");
-const textArea = notesEl.querySelector("textarea");
-
-editBtn.addEventListener("click", () => {
-  main.classList.toggle("hidden");
-  textArea.classList.toggle("hidden");
+addBtn.addEventListener("click", () => {
+  addNewNote();
 });
 
-textArea.addEventListener("input", (e) => {
-  const { value } = e.target;
+function addNewNote() {
+  const note = document.createElement("div");
 
-  main.innerHTML = marked(value);
-});
+  note.classList.add("note");
+
+  note.innerHTML = `
+   <div class="notes">
+    <div class="tools">
+      <button class="edit">
+        <i class="fa-solid fa-pen-to-square"></i>
+      </button>
+      <button class="delete">
+        <i class="fa-regular fa-trash-can"></i>
+      </button>
+  
+      <button>
+        <i class="fa-solid fa-ellipsis"></i>
+       </button>
+      <button>
+        <i class="fa-solid fa-gear"></i>
+      </button>
+      <button>
+        <i class="fa-solid fa-x"></i>
+      </button>
+    </div>
+  
+    <div class="main hidden">
+  
+    </div>
+    <textarea></textarea>
+   </div>
+  `;
+
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+  
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+  
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+  
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+  
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+  
+    main.innerHTML = marked(value);
+  });
+
+  document.body.appendChild(note);
+}
+
